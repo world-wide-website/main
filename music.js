7fb5bbcc7f1974d3aa3a7f7a1e75d659bdd3853c
amigo-musics <157046388+amigo-musics@users.noreply.github.com>
2024-01-18 23:40:10 +0530

    Added music.js file

---
+++ music.js
@@ -0,0 +1,14 @@
+let songIndex = 0;
+
+const songs = [
+  'ROOP TERA X MOOD',
+  'Bandya',
+  'Coca Cola',
+  'DAKU',
+  'Kindess',
+  'Old Town Road x No Love',
+  'First Class',
+  'ANIMAL'
+];
+
+loadSong(songs[songIndex]);
