169eabcfd1e29a5f6b07540f43fec0e8cc1a4e5e
Amigo Music <157046388+amigo-musics@users.noreply.github.com>
2024-03-21 00:04:41 +0530

    Update music.js

--- music.js
+++ music.js
@@ -1,13 +1,13 @@
 let songIndex = 0;
 
 const songs = [
+  'First-Class',
   'ROOP-TERA-X-MOOD',
   'Bandya',
   'Coca-Cola',
   'DAKU',
   'Kindess',
   'Old-Town-Road-x-No-Love',
-  'First-Class',
   'ANIMAL'
 ];
 
