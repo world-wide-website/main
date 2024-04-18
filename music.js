dd7da2511678e75b228e8f8ac65309db57101ed4
Prudhvi Raj <157046388+Amigo-I@users.noreply.github.com>
2024-04-19 02:58:20 +0530

    Update music.js

--- music.js
+++ music.js
@@ -2,19 +2,13 @@ let songIndex = 0;
 
 const songs = [
   'Deo-Deo',
-  'Centuries',
-  'abcdef',
   'Infinity',
-  'Mera_safar',
   'Safar',
-  'First-Class',
-  'First-Class',
   'ROOP-TERA-X-MOOD',
   'Bandya',
   'Coca-Cola',
   'DAKU',
   'Kindess',
-  'Old-Town-Road-x-No-Love',
   'ANIMAL'
 ];
 
