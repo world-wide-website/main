34cb8761f5ba2cd0b34fcc38756169e8ca626645
Prudhvi Raj <157046388+Amigo-I@users.noreply.github.com>
2024-05-13 09:06:05 +0530

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const script = document.createElement("script");
+      myapp.src = "script.js";
+document.body.appendChild(script);
+
+const timer = document.createElement("script");
+      timer.src = "timer.js";
+document.body.appendChild(timer); 
