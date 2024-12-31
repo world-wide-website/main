fbb294c10a9307d46d824844291b9e31e2efcc2e
Prudhvi Raj <157046388+world-wide-website@users.noreply.github.com>
2024-12-31 17:16:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-const main = document.createElement("script");
-      main.src = "main.js";
-document.body.appendChild(main);
+document.addEventListener('DOMContentLoaded', () => {
+const links = [
+        { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" },
+        { rel: "stylesheet", href: "https://site-assets.fontawesome.com/releases/v6.5.1/css/all.css" },
+        { rel: "stylesheet", href: "style.css" },
+        { rel: "text/javascript", src: "main.js"},
+        { rel: "text/javascript", src: "audios.js"},
+        { rel: "text/javascript", src: "music.js"}
+    ];
 
-const audios = document.createElement("script");
-      audios.src = "audios.js";
-document.body.appendChild(audios); 
-
-const music = document.createElement("script");
-      music.src = "music.js";
-document.body.appendChild(music);
+links.forEach(({ rel, href, src }) => {
+    const link = document.createElement(rel === "text/javascript" ? "script" : "link");
+        if (rel === "text/javascript") {link.src = src;link.type = "text/javascript";document.body.appendChild(link);}
+        else {link.rel = rel;link.href = href;document.head.appendChild(link);}
+    });
+      
+    const fastTag = (tag, parent, className, id, text) => {
+        const el = document.createElement(tag);
+        if (className) el.className = className;
+        if (id) el.id = id;
+        if (text) el.textContent = text;
+        if (parent) parent.appendChild(el);
+        return el;
+    };
+});
