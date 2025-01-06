8f4917c551c82d1a99a752dc97d09e99000db238
Prudhvi Raj <157046388+world-wide-website@users.noreply.github.com>
2025-01-07 02:29:11 +0530

    Update and rename audios.js to script/audios.js

---
+++ script/audios.js
@@ -0,0 +1,11 @@
+const Audios = [
+    { Type: 'mp3', Title: 'Animal', Src: 'ANIMAL', Img: 'BGM ', imgType: 'png', 
+         Tags: [ 'BGM', 'song', 'Hindii', 'Remix'] 
+    },
+    { Type: 'mp3', Title: 'Daku', Src: 'DAKU', Img: 'BGM', imgType: 'png', 
+         Tags: ['song', 'leader'] 
+    },
+    { Type: 'mp3', Title: 'Deo Deo', Src: 'Deo-Deo', Img: 'BGM', imgType: 'png', 
+         Tags: ['song', 'Telugu', 'Item Song'] 
+    },
+];
