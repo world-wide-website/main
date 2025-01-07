eb1ccc8115dee3361ecbc5c29275b873b747cff8
Prudhvi Raj <157046388+world-wide-website@users.noreply.github.com>
2025-01-07 09:44:53 +0530

    Update and rename script/videos.js to videos.js

--- script/videos.js
+++
@@ -1,5 +0,0 @@
-const Videos = [
-    { Type: 'mp4', Title: 'Jack Sparrow', Src: 'Jack sparrow', Img: 'BGM music', imgType: 'png', 
-         Tags: ['vidio', 'action'] 
-    },
-];
---
+++ videos.js
@@ -0,0 +1,11 @@
+const Videos = [
+    { Type: 'mp4', Title: 'Jack Sparrow', Src: 'Jack sparrow', Img: 'BGM music', imgType: 'png', 
+         Tags: ['vidio', 'action'] 
+    },
+    { Type: 'mp4', Title: 'Da da da', Src: 'Da da da', Img: 'BGM music', imgType: 'png', 
+         Tags: ['vidio', 'shorts', 'singer'] 
+    },
+    { Type: 'mp4', Title: 'Chaiya Chaiya', Src: 'Chaiya Chaiya', Img: 'BGM music', imgType: 'png', 
+         Tags: ['vidio', 'action'] 
+    },
+];
